5f45b33df859b9031589378dde7b387ba398b25d
mfunyu <mfunyu@student.42tokyo.jp>
2022-06-14 19:27:04 +0900

    update: style use variable

--- pre-trans-app/src/App.tsx
+++ pre-trans-app/src/App.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 function App() {
   const onClickButton = () => alert("Button clicked");
+  const contentStyle = {
+    color: "#00babc",
+    fontSize: "20px",
+  };
   return (
     <>
       <h1>Hello World!</h1>
-      <p style={{ color: "#00babc", fontSize: "20px" }}>42Tokyo : Japan</p>
+      <p style={contentStyle}>42Tokyo : Japan</p>
       <button onClick={onClickButton}>Click here :)</button>
     </>
   );
