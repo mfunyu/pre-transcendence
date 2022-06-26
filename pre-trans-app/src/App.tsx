d7b848971584a9eabfb78ce8d7d40c8528b223f7
mfunyu <mfunyu@student.42tokyo.jp>
2022-06-26 19:29:41 +0900

    add: useState

--- pre-trans-app/src/App.tsx
+++ pre-trans-app/src/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import StyledMessage from "./components/StyledMessage";
 
 function App() {
   const onClickButton = () => alert("Button clicked");
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1>Hello World!</h1>
-      <StyledMessage color="#00babc">42Tokyo : Japan</StyledMessage>
+      <StyledMessage color="#00babc">{num}Tokyo : Japan</StyledMessage>
       <button onClick={onClickButton}>Click here :)</button>
     </>
   );
