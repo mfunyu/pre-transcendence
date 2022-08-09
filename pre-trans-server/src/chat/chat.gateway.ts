de91e96ebdb4f7adf32f9f7ba2241deae69c8a7d
mfunyu <mfunyu@student.42tokyo.jp>
2022-08-10 00:35:55 +0900

    add: back - output log on message recieved

--- pre-trans-server/src/chat/chat.gateway.ts
+++ pre-trans-server/src/chat/chat.gateway.ts
@@ -1,9 +1,13 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway {
+  private logger: Logger = new Logger('Gateway Log');
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
+    this.logger.log('message recieved');
     return 'Hello world!';
   }
 }
